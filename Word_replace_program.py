2407850f93a7c6cda0257eae2b6bdffee944bfcd
Hatice Özbolat <haticeozbolat17@gmail.com>
2022-11-20 17:57:00 +0300

    The replace() method replaces a specified phrase with another specified phrase

---
+++ Word_replace_program.py
@@ -0,0 +1,10 @@
+#  Use to method: "Replace"
+
+def replace_word():
+    raw_str = " Hi guys,I'm Hatice and I love AI and data "
+    word_to_replace = input("Enter the word to replace: ")
+    word_replacemet = input("Enter the word replacemet: ")
+    print(raw_str.replace(word_to_replace, word_replacemet))
+
+
+replace_word()
